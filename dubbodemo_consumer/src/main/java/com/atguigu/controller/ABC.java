05e53089fc54d3e43ba12d0a1f30a6acea4adcef
Seraphim-LQ <62699769+Seraphim-LQ@users.noreply.github.com>
2021-12-16 16:36:48 +0800

    Update ABC.java

--- dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
@@ -7,11 +7,11 @@ package com.atguigu.controller;
 public class ABC {
     public static void main(String[] args) {
         System.out.println("111111111master");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
+        System.out.println("111111111DEV111");
+        System.out.println("111111111DEV111");
+        System.out.println("111111111DEV111");
+        System.out.println("111111111DEV111");
+        System.out.println("111111111DEV111");
+        System.out.println("111111111DEV111");
     }
 }
