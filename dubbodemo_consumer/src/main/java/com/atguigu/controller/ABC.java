c5f371bf059df63bc5103ddd8e1ae2fc40b23c07
liuqing <1411421812@qq.com>
2021-12-16 15:32:08 +0800

    新增一个ABC的类

---
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
@@ -0,0 +1,11 @@
+package com.atguigu.controller;
+
+/**
+ * @author shkstart
+ * @create 2021-12-16 15:28
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+    }
+}
